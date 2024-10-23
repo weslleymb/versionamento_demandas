96cdca5bd9d019887d583e1c40fc028b4cab4eec
weslley <weslley.barbosa@grupoboticario.com.br>
2024-10-23 16:30:33 -0300

    refactor: alteracao origem

--- tb_abrangencia/create_tb_abrangencia.sql
+++ tb_abrangencia/create_tb_abrangencia.sql
@@ -1,8 +1,7 @@
-CREATE TABLE `teste.tb_abrangencia456876` (
+CREATE OR REPLACE TABLE `teste.tb_abrangencia456876` (
     chave             STRING,
     codigo            STRING,
     flg_trg           BOOLEAN,
     flg_bnf           BOOLEAN,
-    percentual_ideal  NUMERIC,
-    catalogo          BOOLEAN
+    percentual_ideal  NUMERIC
 );
